c9670b1d6426b34187ce5a94ebf006a854c9bafd
Jose Stricklin <josestricklin@El-Diablo.local>
2019-01-03 18:03:39 -0700

    working basic data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessComponent } from './success/success.component';
+import { WarningComponent } from './warning/warning.component';
 
 @NgModule({
   declarations: [
       AppComponent,
       ServerComponent,
       ServersComponent,
+      SuccessComponent,
+      WarningComponent,
   ],
   imports: [
       HttpModule,
---
+++ src/app/warning/warning.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-warning',
+    styles: [`
+        p {
+            background: orange;
+            font-weight: 700;
+        }
+        `],
+    templateUrl: './warning.component.html'
+})
+export class WarningComponent {
+
+}
